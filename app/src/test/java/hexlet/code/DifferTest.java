ce9bf6f11f17a24d155576c9075c66a093501145
Alexey Vasilev <davetmsq@gmail.com>
2024-03-31 18:40:59 +0500

    Step6: workflows test

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -13,8 +13,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class DifferTest {
-    static File firstJsonFile = new File("src/test/resources/1.json");
-    static File secondJsonFile = new File("src/test/resources/2.json");
+//    static File firstJsonFile = new File("src/test/resources/1.json");
+//    static File secondJsonFile = new File("src/test/resources/2.json");
+    static Path firstJsonFile = Paths.get("src/test/resources/1.json").toAbsolutePath();
+    static Path secondJsonFile = Paths.get("src/test/resources/2.json").toAbsolutePath();
 
     @BeforeAll
     public static void filesInit() throws IOException {
@@ -30,14 +32,17 @@ public class DifferTest {
                 + "  \"host\": \"hexlet.io\"\n"
                 + "}";
 
-        Files.writeString(firstJsonFile.toPath().toAbsolutePath(), json1);
-        Files.writeString(secondJsonFile.toPath().toAbsolutePath(), json2);
+        Files.writeString(firstJsonFile, json1);
+        Files.writeString(secondJsonFile, json2);
+
+//        Files.writeString(firstJsonFile.toPath().toAbsolutePath(), json1);
+//        Files.writeString(secondJsonFile.toPath().toAbsolutePath(), json2);
     }
 
     @AfterAll
     public static void filesDelete() {
-        firstJsonFile.delete();
-        secondJsonFile.delete();
+        firstJsonFile.toFile().delete();
+        secondJsonFile.toFile().delete();
     }
 
     @Test
@@ -50,9 +55,10 @@ public class DifferTest {
                 + "  + timeout: 20\n"
                 + "  + verbose: true\n"
                 + "}";
-        Path path1 = Paths.get(firstJsonFile.getAbsolutePath());
-        Path path2 = Paths.get(secondJsonFile.getAbsolutePath());
-        String actual = Differ.generate(path1, path2);
+//        Path path1 = Paths.get(firstJsonFile.getAbsolutePath());
+//        Path path2 = Paths.get(secondJsonFile.getAbsolutePath());
+//        String actual = Differ.generate(path1, path2);
+        String actual = Differ.generate(firstJsonFile, secondJsonFile);
 
         assertEquals(expected, actual);
     }
